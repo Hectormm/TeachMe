110ee3fee6e915eeabceeb598e7dcc2f9a791ed2
Carlos Antón Herrera <carlos.antonh@gmail.com>
2014-08-25 14:36:58 +0200

    Cerrar
    Botón cerrar app

--- TeachMe/MainWindow.xaml.cs
+++ TeachMe/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace TeachMe
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Close App
+            Application.Current.Shutdown();
+        }
     }
 }
