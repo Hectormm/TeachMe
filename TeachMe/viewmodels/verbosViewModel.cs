a82dd303456ce7ef22709abfaeb0ee490a5422ba
Carlos Antón Herrera <carlos.antonh@gmail.com>
2014-08-26 14:17:16 +0200

    Comienzo de Dictionary

---
+++ TeachMe/viewmodels/verbosViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonMvvm;
+
+namespace TeachMe.viewmodels
+{
+    class verbosViewModel : ObservableObject, IDataErrorInfo
+    {
+        private Dictionary<String, List<String>> dictionary;
+
+        public verbosViewModel()
+        {
+            rellenarDictionary(dictionary);
+        }
+
+        #region Commandss
+
+        public void rellenarDictionary(Dictionary<String, List<String>> dictionary)
+        {
+            string[] array = new string[3];
+
+            List<String> hola = new List<string>();
+
+            hola.Add("hello");
+            hola.Add("hi");
+            hola.Add("hallo");
+            
+
+            dictionary.Add("hola", hola);
+        }
+
+        #endregion
+    }
+}
